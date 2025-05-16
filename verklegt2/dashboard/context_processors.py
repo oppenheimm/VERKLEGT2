3270f4d5a017cd53b978829934da8a9915d277ef
Gu-wop <aronkkristjansson@gmail.com>
2025-05-16 10:31:44 +0000

    Update context_processors.py

--- verklegt2/dashboard/context_processors.py
+++ verklegt2/dashboard/context_processors.py
@@ -3,9 +3,7 @@ from property.models import PurchaseOffer
 def notification_context(request):
     has_notifications = False
     if request.user.is_authenticated:
-        has_notifications = PurchaseOffer.objects.filter(property__owner=request.user).exists()
-        
-        if not has_notifications:
-            has_notifications = PurchaseOffer.objects.filter(buyer=request.user).exists()
-        
+        seller_has = PurchaseOffer.objects.filter(property__owner=request.user).exists()
+        buyer_has = PurchaseOffer.objects.filter(buyer=request.user).exists()
+        has_notifications = seller_has or buyer_has
     return {'has_notifications': has_notifications}
